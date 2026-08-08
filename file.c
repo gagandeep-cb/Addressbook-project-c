b12d119fe5dbeffd9e98c9706ed4122fd6aeca7d
gagandeep-cb <gagandeepbanakar@gmail.com>
2026-08-08 16:45:53 +0000

    Complete address book functionality

--- file.c
+++ file.c
@@ -1,18 +1,60 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "file.h"
 
-void saveContactsToFile(AddressBook *addressBook) {
+void saveContactsToFile(AddressBook *addressBook)
+{
     FILE *fp;
-    fp=fopen("contacts.txt","w");
-    fprintf(fp,"#%d\n",addressBook->contactCount);
-    for(int i=0;i<addressBook->contactCount;i++)
+
+    fp = fopen("contacts.csv", "w");
+
+    if (fp == NULL)
     {
-         fprintf(fp, "%s,%s,%s\n",addressBook->contacts[i].name,addressBook->contacts[i].phone,addressBook->contacts[i].email);
+        perror("Error opening file");
+        return;
     }
+
+    fprintf(fp, "#%d\n", addressBook->contactCount);
+
+    for (int i = 0; i < addressBook->contactCount; i++)
+    {
+        fprintf(fp, "%s,%s,%s\n",
+                addressBook->contacts[i].name,
+                addressBook->contacts[i].phone,
+                addressBook->contacts[i].email);
+    }
+
     fclose(fp);
-  
+
+    for (int i = 0; i < 100; i++)
+    {
+        printf("[Saving.........................%d%%]\r", i);
+        usleep(10000);
+        fflush(stdout);
+    }
 }
 
-void loadContactsFromFile(AddressBook *addressBook) {
-    
+void loadContactsFromFile(AddressBook *addressBook)
+{
+    FILE *fp;
+
+    fp = fopen("contacts.csv", "r");
+
+    if (fp == NULL)
+    {
+        perror("Error opening file");
+        return;
+    }
+
+    fscanf(fp, "#%d\n", &addressBook->contactCount);
+
+    for (int i = 0; i < addressBook->contactCount; i++)
+    {
+        fscanf(fp, " %[^,],%[^,],%[^\n]\n",
+               addressBook->contacts[i].name,
+               addressBook->contacts[i].phone,
+               addressBook->contacts[i].email);
+    }
+
+    fclose(fp);
 }
--- main.c
+++ main.c
@@ -1,53 +1,66 @@
 #include <stdio.h>
+#include<unistd.h>
 #include "contact.h"
+#include "file.h"
 
-
-int main() 
+int main()
 {
+    for (int i = 0; i <= 100; i++)
+    {
+        printf("[Loading.........................%d%%]\r",i);
+        usleep(10000);
+        fflush(stdout);
+        
+    }
     int choice;
     AddressBook addressBook;
 
-    addressBook.contactCount = 0;
-    //initialize(&addressBook); // Initialize the address book
+    initialize(&addressBook); // Initialize the address book
 
-    do 
+    do
     {
-        printf("\nAddress Book Menu:\n");
-        printf("1. Create contact\n");
-        printf("2. Search contact\n");
-        printf("3. Edit contact\n");
-        printf("4. Delete contact\n");
-        printf("5. List all contacts\n");
-    	printf("6. Save and Exit\n");		
-        // printf("7. Exit\n");
+        printf("\n\n------------------------------------\n");
+        printf("|           ADDRESSBOOK MENU       |\n");
+        printf("------------------------------------\n");
+        printf("|   1   |   %-22s |\n", "CREATE CONTACT");
+        printf("|   2   |   %-22s |\n", "SEARCH CONTACT");
+        printf("|   3   |   %-22s |\n", "EDIT CONTACT");
+        printf("|   4   |   %-22s |\n", "DELETE CONTACT");
+        printf("|   5   |   %-22s |\n", "LIST ALL CONTACTS");
+        printf("|   6   |   %-22s |\n", "SAVE AND EXIT");
+        printf("|   7   |   %-22s |\n", "EXIT");
+        printf("------------------------------------\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
-        
-        switch (choice) 
+
+        switch (choice)
         {
-            case 1:
-                createContact(&addressBook);
-                break;
-            case 2:
-                searchContact(&addressBook);
-                break;
-            case 3:
-                 editContact(&addressBook);
-                break;
-            case 4:
-                 deleteContact(&addressBook);
-                break;
-            case 5:          
-                 listContacts(&addressBook);
-                break;
-            case 6:
-                printf("Saving and Exiting...\n");
-                saveContactsToFile(&addressBook);
-                break;
-            default:
-                printf("Invalid choice. Please try again.\n");
+        case 1:
+            createContact(&addressBook);
+            break;
+        case 2:
+            searchContact(&addressBook);
+            break;
+        case 3:
+            editContact(&addressBook);
+            break;
+        case 4:
+            deleteContact(&addressBook);
+            break;
+        case 5:
+            listContacts(&addressBook);
+            break;
+        case 6:
+            printf("Saving and Exiting...\n");
+            saveContactsToFile(&addressBook);
+            break;
+        case 7:
+            printf("Exit\n");
+            return 0;
+        default:
+            printf("Invalid choice. Please try again.\n");
         }
     } while (choice != 6);
-    
+
     return 0;
 }
